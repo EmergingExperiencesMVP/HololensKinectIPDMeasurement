fccd94fb47e98471c9d919139783a080c8e9cf27
jamesashley1 <jamesashley@comcast.net>
2016-05-09 07:43:50 -0400

    first check-in. working code.

---
+++ HololensIPDMeasurementTool/ImageExtensions.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Drawing;
+using Microsoft.Kinect;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.IO;
+using Media = System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Threading.Tasks;
+
+namespace ImaginativeUniversal
+{
+    public static class KinectImageExtensions
+    {
+        /// <summary>
+        /// Deletes the object.
+        /// </summary>
+        /// <param name="o">The o.</param>
+        /// <returns></returns>
+        [DllImport("gdi32")]
+        private static extern int DeleteObject(IntPtr o);
+
+
+        public static Media.ImageSource ToBitmap(this ColorFrame frame)
+        {
+            int width = frame.FrameDescription.Width;
+            int height = frame.FrameDescription.Height;
+
+            byte[] pixels = new byte[width * height * ((Media.PixelFormats.Bgr32.BitsPerPixel + 7) / 8)];
+
+            if (frame.RawColorImageFormat == ColorImageFormat.Bgra)
+            {
+                frame.CopyRawFrameDataToArray(pixels);
+            }
+            else
+            {
+                frame.CopyConvertedFrameDataToArray(pixels, ColorImageFormat.Bgra);
+            }
+
+            int stride = width * Media.PixelFormats.Bgr32.BitsPerPixel / 8;
+
+            return BitmapSource.Create(width, height, 96, 96, Media.PixelFormats.Bgr32, null, pixels, stride);
+        }
+
+        public static Media.ImageSource ToBitmap(this InfraredFrame frame)
+        {
+            int width = frame.FrameDescription.Width;
+            int height = frame.FrameDescription.Height;
+
+            ushort[] infraredData = new ushort[width * height];
+            byte[] pixelData = new byte[width * height * (Media.PixelFormats.Bgr32.BitsPerPixel + 7) / 8];
+
+            frame.CopyFrameDataToArray(infraredData);
+
+            int colorIndex = 0;
+            for (int infraredIndex = 0; infraredIndex < infraredData.Length; ++infraredIndex)
+            {
+                ushort ir = infraredData[infraredIndex];
+                byte intensity = (byte)(ir >> 8);
+
+                pixelData[colorIndex++] = intensity; // Blue
+                pixelData[colorIndex++] = intensity; // Green   
+                pixelData[colorIndex++] = intensity; // Red
+
+                ++colorIndex;
+            }
+
+            int stride = width * Media.PixelFormats.Bgr32.BitsPerPixel / 8;
+
+            return BitmapSource.Create(width, height, 96, 96, Media.PixelFormats.Bgr32, null, pixelData, stride);
+        }
+
+        public static Media.ImageSource ToBitmap(this DepthFrame frame)
+        {
+            int width = frame.FrameDescription.Width;
+            int height = frame.FrameDescription.Height;
+
+            ushort minDepth = frame.DepthMinReliableDistance;
+            ushort maxDepth = frame.DepthMaxReliableDistance;
+
+            ushort[] depthData = new ushort[width * height];
+            byte[] pixelData = new byte[width * height * (Media.PixelFormats.Bgr32.BitsPerPixel + 7) / 8];
+
+            frame.CopyFrameDataToArray(depthData);
+
+            int colorIndex = 0;
+            for (int depthIndex = 0; depthIndex < depthData.Length; ++depthIndex)
+            {
+                ushort depth = depthData[depthIndex];
+                byte intensity = (byte)(depth >= minDepth && depth <= maxDepth ? depth : 0);
+
+                pixelData[colorIndex++] = intensity; // Blue
+                pixelData[colorIndex++] = intensity; // Green
+                pixelData[colorIndex++] = intensity; // Red
+
+                ++colorIndex;
+            }
+
+            int stride = width * Media.PixelFormats.Bgr32.BitsPerPixel / 8;
+
+            return BitmapSource.Create(width, height, 96, 96, Media.PixelFormats.Bgr32, null, pixelData, stride);
+        }
+
+        public static double Distance(this CameraSpacePoint v1, CameraSpacePoint v2)
+        {
+            return
+               Math.Sqrt
+               (
+                   (v1.X - v2.X) * (v1.X - v2.X) +
+                   (v1.Y - v2.Y) * (v1.Y - v2.Y) +
+                   (v1.Z - v2.Z) * (v1.Z - v2.Z)
+               );
+        }
+    }
+}
---
+++ HololensIPDMeasurementTool/MainWindow.xaml.cs
@@ -0,0 +1,263 @@
+﻿using Microsoft.Kinect;
+using Microsoft.Kinect.Face;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ImaginativeUniversal;
+
+
+namespace HololensIPDMeasurementTool
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private KinectSensor _sensor = null;
+
+        private const float InfraredSourceValueMaximum = (float)ushort.MaxValue;
+
+        private const float InfraredSourceScale = 0.75f;
+
+        private const float InfraredOutputValueMinimum = 0.01f;
+
+        private const float InfraredOutputValueMaximum = 1.0f;
+
+        private FrameDescription _infraredFrameDescription = null;
+
+        private BodyFrameReader _bodyReader = null;
+
+        private HighDefinitionFaceFrameSource _faceFrameSource = null;
+
+        private HighDefinitionFaceFrameReader _faceFrameReader = null;
+
+        private CoordinateMapper _coordinateMapper = null;
+
+        private FaceModel _faceModel = null;
+
+        FaceAlignment _faceAlignment = null;
+
+        private Body[] _bodies = null;
+
+        private int _bodyCount;
+
+        private InfraredFrameReader _irReader = null;
+
+        private WriteableBitmap _infraredBitmap = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            _sensor = KinectSensor.GetDefault();
+            _coordinateMapper = _sensor.CoordinateMapper;
+
+            if (_sensor != null)
+            {
+                _infraredFrameDescription = _sensor.InfraredFrameSource.FrameDescription;
+                _infraredBitmap = new WriteableBitmap(_infraredFrameDescription.Width, _infraredFrameDescription.Height, 96.0, 96.0, PixelFormats.Gray32Float, null);
+                camera.Source = _infraredBitmap;
+
+                _bodyReader = _sensor.BodyFrameSource.OpenReader();
+                _bodyCount = _sensor.BodyFrameSource.BodyCount;
+                _bodies = new Body[_bodyCount];
+                _bodyReader.FrameArrived += BodyReader_FrameArrived;
+
+                _faceModel = new FaceModel();
+                _faceAlignment = new FaceAlignment();
+
+                _faceFrameSource = new HighDefinitionFaceFrameSource(_sensor);
+                _faceFrameReader = _faceFrameSource.OpenReader();
+                _faceFrameReader.FrameArrived += FaceReader_FrameArrived;
+
+                _irReader = _sensor.InfraredFrameSource.OpenReader();
+                _irReader.FrameArrived += InfraredReader_FrameArrived;
+
+                _sensor.Open();
+            }
+        }
+
+
+        private void Page_Unloaded(object sender, RoutedEventArgs e)
+        {
+
+            if (_faceFrameReader != null)
+            {
+                this._faceFrameReader.Dispose();
+                this._faceFrameReader = null;
+            }
+
+            if (_faceFrameSource != null)
+            {
+                _faceFrameSource = null;
+            }
+
+
+            if (_bodyReader != null)
+            {
+                _bodyReader.Dispose();
+                _bodyReader = null;
+            }
+
+            if (_sensor != null)
+            {
+                _sensor.Close();
+            }
+        }
+
+        private void FaceReader_FrameArrived(object sender, HighDefinitionFaceFrameArrivedEventArgs args)
+        {
+            using (var frame = args.FrameReference.AcquireFrame())
+            {
+                if (frame != null && frame.IsFaceTracked)
+                {
+                    if (_faceModel == null || _faceAlignment == null)
+                        return;
+
+                    frame.GetAndRefreshFaceAlignmentResult(_faceAlignment);
+                    var faceVertices = _faceModel.CalculateVerticesForAlignment(_faceAlignment);
+
+                    var cspLeftEye = GetLeftEye(faceVertices);
+                    var cspRightEye = GetRightEye(faceVertices);
+
+                    var ipd = cspLeftEye.Distance(cspRightEye) * 1000;
+
+
+                    var pointEyeLeft = _coordinateMapper.MapCameraPointToDepthSpace(cspLeftEye);
+                    var pointEyeRight = _coordinateMapper.MapCameraPointToDepthSpace(cspRightEye);
+                    var pointCheekLeft = _coordinateMapper.MapCameraPointToDepthSpace(faceVertices[412]);
+                    var pointCheekRight = _coordinateMapper.MapCameraPointToDepthSpace(faceVertices[933]);
+                    var pointNose = _coordinateMapper.MapCameraPointToDepthSpace(faceVertices[18]);
+                    var pointMouth = _coordinateMapper.MapCameraPointToDepthSpace(faceVertices[10]);
+                    var pointChin = _coordinateMapper.MapCameraPointToDepthSpace(faceVertices[4]);
+                    var pointForehead = _coordinateMapper.MapCameraPointToDepthSpace(faceVertices[28]);
+
+
+                    this.myText.Text = ipd.ToString("0.000");
+
+                    Canvas.SetLeft(eyeLeft, pointEyeLeft.X - eyeLeft.Width / 2.0);
+                    Canvas.SetTop(eyeLeft, pointEyeLeft.Y - eyeLeft.Height / 2.0);
+
+                    Canvas.SetLeft(eyeRight, pointEyeRight.X - eyeRight.Width / 2.0);
+                    Canvas.SetTop(eyeRight, pointEyeRight.Y - eyeRight.Height / 2.0);
+
+                    Canvas.SetLeft(cheekLeft, pointCheekLeft.X - cheekLeft.Width / 2.0);
+                    Canvas.SetTop(cheekLeft, pointCheekLeft.Y - cheekLeft.Height / 2.0);
+
+                    Canvas.SetLeft(cheekRight, pointCheekRight.X - cheekRight.Width / 2.0);
+                    Canvas.SetTop(cheekRight, pointCheekRight.Y - cheekRight.Height / 2.0);
+
+                    Canvas.SetLeft(nose, pointNose.X - nose.Width / 2.0);
+                    Canvas.SetTop(nose, pointNose.Y - nose.Height / 2.0);
+
+                    Canvas.SetLeft(mouth, pointMouth.X - mouth.Width / 2.0);
+                    Canvas.SetTop(mouth, pointMouth.Y - mouth.Height / 2.0);
+
+                    Canvas.SetLeft(chin, pointChin.X - chin.Width / 2.0);
+                    Canvas.SetTop(chin, pointChin.Y - chin.Height / 2.0);
+
+                    Canvas.SetLeft(forehead, pointForehead.X - forehead.Width / 2.0);
+                    Canvas.SetTop(forehead, pointForehead.Y - forehead.Height / 2.0);
+
+
+                }
+            }
+        }
+
+        private CameraSpacePoint GetLeftEye(IReadOnlyList<CameraSpacePoint> vertices)
+        {
+            CameraSpacePoint cameraSpacePoint = vertices[210];
+            CameraSpacePoint cameraSpacePoint2 = vertices[469];
+            return new CameraSpacePoint
+            {
+                X = (cameraSpacePoint2.X + cameraSpacePoint.X) / 2f,
+                Y = (cameraSpacePoint2.Y + cameraSpacePoint.Y) / 2f,
+                Z = (cameraSpacePoint2.Z + cameraSpacePoint.Z) / 2f
+            };
+
+        }
+
+        /// <summary>
+        /// Returns the center of the Right Eye.
+        /// </summary>
+        private CameraSpacePoint GetRightEye(IReadOnlyList<CameraSpacePoint> vertices)
+        {
+
+            CameraSpacePoint cameraSpacePoint = vertices[843];
+            CameraSpacePoint cameraSpacePoint2 = vertices[1117];
+            return new CameraSpacePoint
+            {
+                X = (cameraSpacePoint2.X + cameraSpacePoint.X) / 2f,
+                Y = (cameraSpacePoint2.Y + cameraSpacePoint.Y) / 2f,
+                Z = (cameraSpacePoint2.Z + cameraSpacePoint.Z) / 2f
+            };
+        }
+
+
+
+        private void InfraredReader_FrameArrived(object sender, InfraredFrameArrivedEventArgs args)
+        {
+            using (var frame = args.FrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    using (Microsoft.Kinect.KinectBuffer infraredBuffer = frame.LockImageBuffer())
+                    {
+                        // verify data and write the new infrared frame data to the display bitmap
+                        if (((_infraredFrameDescription.Width * _infraredFrameDescription.Height) == (infraredBuffer.Size / _infraredFrameDescription.BytesPerPixel)) &&
+                            (_infraredFrameDescription.Width == _infraredBitmap.PixelWidth) && (_infraredFrameDescription.Height == _infraredBitmap.PixelHeight))
+                        {
+                            this.ProcessInfraredFrameData(infraredBuffer.UnderlyingBuffer, infraredBuffer.Size);
+                        }
+                    }
+                }
+            }
+        }
+
+        private unsafe void ProcessInfraredFrameData(IntPtr infraredFrameData, uint infraredFrameDataSize)
+        {
+            ushort* frameData = (ushort*)infraredFrameData;
+            _infraredBitmap.Lock();
+            float* backBuffer = (float*)_infraredBitmap.BackBuffer;
+
+            for (int i = 0; i < (int)(infraredFrameDataSize / _infraredFrameDescription.BytesPerPixel); ++i)
+            {
+                backBuffer[i] = Math.Min(InfraredOutputValueMaximum, (((float)frameData[i] / InfraredSourceValueMaximum * InfraredSourceScale) * (1.0f - InfraredOutputValueMinimum)) + InfraredOutputValueMinimum);
+            }
+
+
+            _infraredBitmap.AddDirtyRect(new Int32Rect(0, 0, _infraredBitmap.PixelWidth, _infraredBitmap.PixelHeight));
+            _infraredBitmap.Unlock();
+        }
+
+        private void BodyReader_FrameArrived(object sender, BodyFrameArrivedEventArgs args)
+        {
+            using (var bodyFrame = args.FrameReference.AcquireFrame())
+            {
+                if (bodyFrame != null)
+                {
+                    bodyFrame.GetAndRefreshBodyData(_bodies);
+                    var body = _bodies.Where(b => b.IsTracked).FirstOrDefault();
+                    if (!_faceFrameSource.IsTrackingIdValid && body != null)
+                    {
+                        _faceFrameSource.TrackingId = body.TrackingId;
+                    }
+                }
+            }
+        }
+    }
+}
